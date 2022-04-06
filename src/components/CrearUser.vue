49ba64e540edd5f99c1e58bd484e5a95ce54b85a
efrenHouse <efrenrodas1@gmail.com>
2022-04-06 00:20:53 -0500

    so bad i m trizte

--- src/components/CrearUser.vue
+++ src/components/CrearUser.vue
@@ -1,6 +1,28 @@
 <template>
-    <div>
-        Crear efren
+    <div class="container">
+      <div class="card text-left">
+        <div class="card-header">
+          Empleados
+        </div>
+        <div class="card-body">
+         <form>
+<div class="form-group">
+  <label for=""></label>
+  <input type="text" class="form-control" name="nombre" id="nombre" aria-describedby="helpId" placeholder="">
+  <small id="helpId" class="form-text text-muted">Nombre</small>
+</div>
+<div class="form-group">
+  <label for=""></label>
+  <input type="text" class="form-control" name="correo" id="correo" aria-describedby="helpId" placeholder="">
+  <small id="helpId" class="form-text text-muted">Correo electronico</small>
+</div>
+<div class="btn-group">
+    <button type="button" class="btn btn-primary">Guardar</button>
+    <button type="button" class="btn btn-secondary">Cancelar</button>
+</div>
+         </form>
+        </div>
+      </div>
     </div>
 </template>
 
